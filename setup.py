04b35521d60184cbf3cc0a3653982c4405574559
Cor Zuurmond <jczuurmond@protonmail.com>
2019-11-29 11:35:51 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+
+import setuptools
+
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+
+requirements = []
+setup_requirements = []
+test_requirements = ['pytest']
+extra_requirements = {}
+
+
+setuptools.setup(
+    name='smelly_rats',
+    author='Cor Zuurmond',
+    author_email='jczuurmond@protonmail.com',
+    description='',
+    url='TODO',
+    license='Open source',
+    packages=setuptools.find_packages('smelly_rats'),
+    version='0.1.0',
+    install_requires=requirements,
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    extras_require=extra_requirements,
+)
