dbdb1d6dfaeb40aabff013c113893fee1dbf53ac
Cor Zuurmond <jczuurmond@protonmail.com>
2019-12-06 14:18:46 +0100

    Add loader module

---
+++ smelly_rats/loader.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+
+def load_smelly_rats(path):
+    """
+    Load the smelly rats data.
+
+    Parameters
+    ----------
+    path : string
+        Path to excel file.
+
+    Returns
+    -------
+    (pd.DataFrame, pd.DataFrame) : The (hnmr spectra, target) data frames.
+    """
+    xl = pd.ExcelFile(path)
+
+    hnmr_spectra = (
+        pd.read_excel(xl, sheet_name=0, index_col=0)
+        .rename(columns=lambda col: col.replace("'", ""))
+
+    )
+
+    target = pd.read_excel(xl, sheet_name=1, usecols=range(10, 15))
+
+    return hnmr_spectra, target
