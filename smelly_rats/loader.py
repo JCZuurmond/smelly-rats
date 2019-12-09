1661fdc3ae934f687b1e7fe764d6099cfdc59966
Cor Zuurmond <jczuurmond@protonmail.com>
2019-12-09 16:12:43 +0100

    Remove redundant white line

--- smelly_rats/loader.py
+++ smelly_rats/loader.py
@@ -19,7 +19,6 @@ def load_smelly_rats(path):
     hnmr_spectra = (
         pd.read_excel(xl, sheet_name=0, index_col=0)
         .rename(columns=lambda col: col.replace("'", ""))
-
     )
 
     target = pd.read_excel(xl, sheet_name=1, usecols=range(10, 15))
